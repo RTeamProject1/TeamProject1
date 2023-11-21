55f1bec70b3b17bc21316473f9ace99f5981f2af
hitantjimin <127213894+jimin0625@users.noreply.github.com>
2023-11-21 17:47:28 +0900

    파이어베이스 연동

---
+++ src/filebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_qEuh7Jw63-YH-V4TGWgLWSZYKd0ct1M",
+  authDomain: "teamproject-react.firebaseapp.com",
+  projectId: "teamproject-react",
+  storageBucket: "teamproject-react.appspot.com",
+  messagingSenderId: "558984290729",
+  appId: "1:558984290729:web:ca7a734a690da58fb3e4b8",
+  measurementId: "G-VPZPRVPFVE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+export {db};
